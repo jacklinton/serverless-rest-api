b79170ea50dfed93d62e136b22f4e1f512076373
Jack Linton <jacklinton@mac.com>
2018-06-03 23:27:41 -0400

    Add routes for crud. Create is only functional method.

--- handler.js
+++ handler.js
@@ -8,17 +8,69 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient({
     endpoint: 'http://localhost:8000'
 }); // remove before deploy
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+module.exports.create = (event, context, callback) => {
+  // create a note and put it in the database
+    const data = JSON.parse(event.body);
 
-  callback(null, response);
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Item: {
+        id: uuid.v1(),
+        content: data.content
+      }
+    };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    dynamoDb.put(params, (error) => {
+
+      if (error) {
+        console.error(error);
+        return callback(null, {
+          statusCode: error.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not create the note.'
+        });
+      }
+
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(params.Item)
+      };
+      callback(null, response);
+    });
+};
+
+module.exports.getOne = (event, context, callback) => {
+    // get one note from the database
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Fetch one note.')
+    };
+    callback(null, response);
+};
+
+module.exports.getAll = (event, context, callback) => {
+    // get all notes from the database
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Fetch all notes.')
+    };
+    callback(null, response);
+};
+
+module.exports.update = (event, context, callback) => {
+    // update a note in the database
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Update a note.')
+    };
+    callback(null, response);
+};
+
+module.exports.delete = (event, context, callback) => {
+    // delete a note from the database
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Delete a note.')
+    };
+    callback(null, response);
 };
